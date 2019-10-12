8f6a906e92045a43df157fe2cc6bfdb3c0ffa841
Saban <ulukayasaban@gmail.com>
2019-10-12 15:46:23 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by SABAN2 on 12.10.2019.
+//  Copyright © 2019 SABAN2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
