beab3b1ab882763fe645d82abbab8eb2df99afb0
Saban Ulukaya <ulukayasaban@gmail.com>
2019-10-12 16:09:48 +0300

    first commit

--- GitTestProject/ViewController.swift
+++ GitTestProject/ViewController.swift
@@ -12,9 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("Git")
+        
+        
+        
+        
     }
 
+    
+    
+    
+    
 
 }
 
